2a900699b43004cfcbd61e70d76edd67e10ccd3a
Hesti Yuana Mahardika <hestiyuanamahardika@gmail.com>
2022-09-24 11:25:57 +0700

    first commit

---
+++ src/scripts/components/hero/hero.js
@@ -0,0 +1,69 @@
+import template from '../../utils/dom';
+import html from './hero.html';
+import './hero.css';
+class Hero extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+  set clickEvent(event) {
+    this._clickEvent = event;
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `${template(html)}`;
+  }
+}
+customElements.define('hero-section', Hero);
+
+function showTime() {
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
+
+}
+
+showTime();
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
---
+++ src/scripts/components/popular/popular.js
@@ -0,0 +1,13 @@
+import template from '../../utils/dom';
+import html from './popular.html';
+
+class Popular extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+    render() {
+        this.innerHTML = `${template(html)}`;
+    }
+}
+
+customElements.define('popular-section', Popular);
